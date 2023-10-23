12b75c3bd7fae20da20dbc76f3036cfc39326534
Márcio Krüger <marcio.kgr@gmail.com>
2023-10-23 11:15:00 -0300

    Add project files.

---
+++ NBomberLoadTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using NBomber.Contracts.Stats;
+using NBomber.CSharp; //utilizando o NBombet
+
+using var httpClient = new HttpClient(); //criando client
+
+var scenario = Scenario.Create("Meu Primeiro Teste Carga", async context =>
+{
+    var response = await httpClient.GetAsync("https://google.com.br");
+
+    return response.IsSuccessStatusCode
+        ? Response.Ok()
+        : Response.Fail();
+});
+
+NBomberRunner
+  .RegisterScenarios(scenario)
+  .WithReportFileName("NBomber")
+  .WithReportFolder("NBomber")
+  .WithReportFormats(ReportFormat.Txt, ReportFormat.Csv, ReportFormat.Html, ReportFormat.Md)
+  .Run();
